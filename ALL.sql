1c652b84fc4fb8b4bbacb3a20eab9e5240c4ffa4
Glory Obi <156969377+Belle-Yeoki@users.noreply.github.com>
2025-05-26 14:08:04 +0100

    Delete ALL.sql

--- ALL.sql
+++
@@ -1,8 +0,0 @@
-SELECT *  --This just helps you see how many tables you have got in your Database
-  FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_NAME = 'Customer'
-  --This just helps you see how many tables you have got in your Database
-  SELECT *
-  FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE TABLE_NAME IN ('Customer','Testing');--This helps you see the content of your columns
-  
